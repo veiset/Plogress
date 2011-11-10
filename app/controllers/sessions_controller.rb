ca977b783186ab1db0386c28faee49db4e97da3f
nicho <nicho>
2011-11-10 19:58:14 +0000

    Delete 2
    git-svn-id: http://veiset.org/svn/plog/plogress/trunk@40 1c7b3678-845b-4165-9e0e-983a4629420a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,22 +12,6 @@ class SessionsController < ApplicationController
     @session = Session.find(params[:id])
   end
 
-  # POST /sessions
-  # POST /sessions.json
-  def create
-    @session = Session.new(params[:session])
-
-    respond_to do |format|
-      if @session.save
-        format.html { redirect_to @session, notice: 'Session was successfully created.' }
-        format.json { render json: @session, status: :created, location: @session }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /sessions/1
   # PUT /sessions/1.json
   def update
